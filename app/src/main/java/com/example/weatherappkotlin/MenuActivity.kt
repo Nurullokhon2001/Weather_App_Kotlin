893c30ea3cb34bcdfb23a4ecb472b375d68a5ff6
Nur <olimovnurullo2001@gmail.com>
2022-04-22 09:20:58 +0500

    added MenuActivity.kt

--- app/src/main/java/com/example/weatherappkotlin/MenuActivity.kt
+++ app/src/main/java/com/example/weatherappkotlin/MenuActivity.kt
@@ -2,10 +2,37 @@ package com.example.weatherappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherappkotlin.business.model.GeoCodeModel
+import com.example.weatherappkotlin.view.MenuView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_menu.*
+import moxy.MvpAppCompatActivity
+import java.util.concurrent.TimeUnit
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : MvpAppCompatActivity(),MenuView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        search_field.createObservable()
+            .doOnNext { setLoading(true) }
+            .debounce(700, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                //TODO  (MenuActivity) if(!it.isNullOrEmpty)  presenter.getFavoriteList()
+            }
+
+    }
+
+    override fun setLoading(flag: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun fillPredictionList(data: List<GeoCodeModel>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun fillFavoriteList(data: List<GeoCodeModel>) {
+        TODO("Not yet implemented")
     }
 }
